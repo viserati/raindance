a37e7acb4150b6d820cb876b3fb5c98edd5792b5
Keith Turner <turner@keithturner.com>
2018-01-02 09:20:25 -0700

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def new
+    @post = Post.new
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+
+resources :posts
+get 'posts/index'
+
+
+end
